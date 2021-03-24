28375a5f3f820d7918cd5d4a8dc342fbf0408590
heymatch <match.remo@gmail.com>
2021-03-25 01:21:57 +0800

    Add Setting

--- autotest.bat
+++ autotest.bat
@@ -6,10 +6,11 @@ IF "%1" == "test" ( GOTO TEST )
 IF "%1" == "cmp" ( GOTO CMP )
 IF "%1" == "ans" ( GOTO ANS )
 IF "%1" == "" ( GOTO DONE )
+IF "%2" == "" ( GOTO DONE )
 
 SET /A target=1
 SET /A pass=0
-Skyrmion.exe test\testcase%1.txt test\setting%1.txt > test\output%1.out 2> test\log%1.log
+Skyrmion.exe test\testcase%1.txt test\setting%2.txt > test\output%1.out 2> test\log%1.log
 IF %errorlevel%==0 ( ECHO test\testcase%1.txt ok & SET /A pass=pass+1 )
 REM FC test\answer%1.ans test\output%1.out
 GOTO DONE
@@ -20,7 +21,7 @@ SET /A pass=0
 FOR /l %%a IN (1 1 15) DO (
     REM COPY test\output%%a.out test\past%%a.out 
     ECHO Test test\testcase%%a.txt ...
-    Skyrmion.exe test\testcase%%a.txt test\setting%%a.txt > test\output%%a.out 2> test\log%%a.log
+    Skyrmion.exe test\testcase%%a.txt test\setting%2.txt > test\output%%a.out 2> test\log%%a.log
     IF %errorlevel%==0 ( ECHO test\testcase%%a.txt ok & SET /A pass=pass+1 )
     ECHO Finish test\testcase%%a.txt
 )
@@ -28,14 +29,14 @@ GOTO DONE
 
 :CMP
 FOR /l %%a IN (1 1 17) DO (
-    Skyrmion.exe test\testcase%%a.txt test\setting%%a.txt > test\output%%a.out 
+    Skyrmion.exe test\testcase%%a.txt test\setting%2.txt > test\output%%a.out 
     FC test\answer%%a.ans test\output%%a.out
 )
 GOTO DONE
 
 :ANS
 FOR /l %%a IN (1 1 17) DO (
-    @Skyrmion.exe test\testcase%%a.txt test\setting%%a.txt > test\answer%%a.ans 
+    @Skyrmion.exe test\testcase%%a.txt test\setting%2.txt > test\answer%%a.ans 
 )
 GOTO DONE
 
--- quick.bat
+++ quick.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 MKDIR obj
 make
-autotest %1
+autotest %1 %2
