7ed805e16c36a78bc524e664ea8feb6f8e9d8a94
heymatch <match.remo@gmail.com>
2021-04-28 16:27:27 +0800

    Update autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -3,4 +3,4 @@ if [ "$1" == "" ] || [ "$2" == "" ]; then
     exit
 fi
 make
-./Skyrmion.out test/testcase$1.txt test/setting$2.txt test/output$1.dat 2> test/log$1.log
+./Skyrmion.out test/testcase$1.txt benchmark/setting$2.txt test/output$1.dat test/nodeInfo$1.csv test/treeHeight$1.csv 2> test/log$1.log
