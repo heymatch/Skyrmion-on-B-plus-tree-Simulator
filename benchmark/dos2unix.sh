f378b5fcd75c903e38dc2d170e394a636a565d23
heymatch <match.remo@gmail.com>
2021-04-29 02:34:38 +0800

    Create dos2unix.sh

---
+++ benchmark/dos2unix.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+dos2unix tpch5.in
+dos2unix tpch10.in
+dos2unix tpch50.in
+dos2unix tpch100.in
+
+dos2unix zipfian5.in
+dos2unix zipfian10.in
+dos2unix zipfian50.in
+dos2unix zipfian100.in
+
+dos2unix uniform5.in
+dos2unix uniform10.in
+dos2unix uniform50.in
+dos2unix uniform100.in
+
+dos2unix hotspot5.in
+dos2unix hotspot10.in
+dos2unix hotspot50.in
+dos2unix hotspot100.in
+
+dos2unix sequential5.in
+dos2unix sequential10.in
+dos2unix sequential50.in
+dos2unix sequential100.in
+
+dos2unix exponential5.in
+dos2unix exponential0.in
+dos2unix exponential50.in
+dos2unix exponential100.in
+
+dos2unix latest5.in
+dos2unix latest10.in
+dos2unix latest50.in
+dos2unix latest100.in
