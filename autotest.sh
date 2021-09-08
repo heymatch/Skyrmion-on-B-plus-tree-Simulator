125833e26d9f795f9d48e876c0704c4b44872393
heymatch <match.remo@gmail.com>
2021-09-08 13:12:47 +0800

    Fix Script

---
+++ autotest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ "$1" == "" ] || [ "$2" == "" ]; then 
+    exit
+fi
+make
+./Skyrmion test/testcase/testcase$1.txt benchmark/setting/setting$2.txt testcase${1}_setting${2} test/ 2> test/log$1.log
--- test/autotest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$1" == "" ] || [ "$2" == "" ]; then 
-    exit
-fi
-make
-./Skyrmion test/testcase$1.txt benchmark/setting/setting$2.txt testcase${1}_setting${2} test/ 2> test/log$1.log
