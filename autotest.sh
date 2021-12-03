57a299cbe4830bea00407ad90fb1b1f5c57786ee
heymatch <match.remo@gmail.com>
2021-12-03 16:59:03 +0800

    Update autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -2,5 +2,6 @@
 if [ "$1" == "" ] || [ "$2" == "" ]; then 
     exit
 fi
+
 make
 ./Skyrmion test/testcase/testcase$1.txt benchmark/setting/setting$2.txt testcase${1}_setting${2} test/ 2> test/log$1.log
